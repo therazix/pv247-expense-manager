12f6d6f6d57824b1ddfd8e046d7b09402da7d979
Jana Stopková <jana.stopkova@intecs.cz>
2023-12-05 17:39:07 +0100

    feat: modify category schemas and repository

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,9 +1,13 @@
 import { type z } from 'zod';
 
 import {
+	type categoryCreateSchemaWithoutUserId,
 	type categoryCreateSchema,
 	type categorySchema
 } from '@/validators/category';
 
 export type Category = z.infer<typeof categorySchema>;
+export type NewCategoryWithoutUserId = z.infer<
+	typeof categoryCreateSchemaWithoutUserId
+>;
 export type NewCategory = z.infer<typeof categoryCreateSchema>;
