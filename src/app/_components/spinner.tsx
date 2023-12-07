a48d14046e6e8163fab329ebf9db5836900549ce
Filip Vágner <therazix@gmail.com>
2023-12-07 18:41:40 +0100

    Center loading spinner position

--- src/app/_components/spinner.tsx
+++ src/app/_components/spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner = () => (
 	<div className="absolute left-0 top-[85px] h-full w-full backdrop-blur-sm lg:left-[260px] lg:top-0 lg:w-[calc(100%-260px)]">
-		<div className="border-gray-200 relative left-[50%] top-[50%] h-20 w-20 animate-spin rounded-full border-8 border-t-majorelle-blue" />
+		<div className="border-gray-200 relative left-[calc(50%-40px)] top-[50%] h-20 w-20 animate-spin rounded-full border-8 border-t-majorelle-blue" />
 	</div>
 );
 
