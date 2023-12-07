7e3f74815842317cfcde2d0ecbb31358671552d6
Filip Vágner <therazix@gmail.com>
2023-12-07 13:29:58 +0100

    Add financialAccountSchemaWithoutUserId, Refactor schema

--- src/types/financial-account.ts
+++ src/types/financial-account.ts
@@ -2,8 +2,12 @@ import { type z } from 'zod';
 
 import {
 	type financialAccountCreateSchema,
+	type financialAccountCreateSchemaWithoutUserId,
 	type financialAccountSchema
 } from '@/validators/financial-account';
 
 export type FinancialAccount = z.infer<typeof financialAccountSchema>;
 export type NewFinancialAccount = z.infer<typeof financialAccountCreateSchema>;
+export type NewFinancialAccountWithoutUserId = z.infer<
+	typeof financialAccountCreateSchemaWithoutUserId
+>;
