755b6c6b5e423b2ad5fff49016427d7335e8e50b
Filip Vágner <therazix@gmail.com>
2023-12-09 00:27:44 +0100

    Add metadata, change favicon

--- src/app/categories/layout.tsx
+++ src/app/categories/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
+
 import CategoryProviders from './categoryProviders';
+
+export const metadata: Metadata = {
+	title: 'Categories'
+};
 
 const CategoryLayout = ({ children }: { children: React.ReactNode }) => (
 	<CategoryProviders>
