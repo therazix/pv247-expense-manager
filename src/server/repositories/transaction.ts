6a37e19e4d917ef67fe18ecb925e2c7a9b5fda67
Jana Stopková <jana.stopkova@intecs.cz>
2023-12-11 18:04:08 +0100

    feat: extend transaction repository

--- src/server/repositories/transaction.ts
+++ src/server/repositories/transaction.ts
@@ -1,5 +1,9 @@
 import { transactionSchema } from '@/validators/transaction';
-import { type NewTransaction } from '@/types/transaction';
+import {
+	type TransactionSearchParams,
+	type NewTransaction,
+	type Transaction
+} from '@/types/transaction';
 
 import { db } from '../db';
 
@@ -36,15 +40,39 @@ export const createTransaction = async (transaction: NewTransaction) => {
 	return transactionSchema.parse(newTransaction);
 };
 
+export const updateTransaction = async (transaction: Transaction) => {
+	const updatedTransaction = await db.transaction.update({
+		where: { id: transaction.id },
+		data: {
+			name: transaction.name,
+			description: transaction.description,
+			amount: transaction.amount,
+			datetime: transaction.datetime,
+			financialAccountId: transaction.financialAccountId,
+			categoryId: transaction.categoryId
+		}
+	});
+	return transactionSchema.parse(updatedTransaction);
+};
+
 export const deleteTransaction = async (id: string) => {
 	const transaction = await db.transaction.findUnique({
-		where: { id },
-		include: { financialAccount: true }
+		where: { id }
 	});
 	if (!transaction) throw new Error('Transaction not found');
 
+	const deletedTransaction = deleteTransactionTransaction(
+		transaction as Transaction
+	);
+	return transactionSchema.parse(deletedTransaction);
+};
+
+const deleteTransactionTransaction = async (transaction: Transaction) => {
 	const [deletedTransaction, _financialAcc] = await db.$transaction([
-		db.transaction.delete({ where: { id }, include: { category: true } }),
+		db.transaction.delete({
+			where: { id: transaction.id },
+			include: { category: true }
+		}),
 		db.financialAccount.update({
 			where: { id: transaction.financialAccountId },
 			data: {
@@ -54,7 +82,28 @@ export const deleteTransaction = async (id: string) => {
 			}
 		})
 	]);
-	return transactionSchema.parse(deletedTransaction);
+
+	return deletedTransaction;
+};
+
+export const deleteTransactions = async (ids: string[]) => {
+	const transactions = await db.transaction.findMany({
+		where: { id: { in: ids } },
+		include: { financialAccount: true }
+	});
+	if (!transactions) throw new Error('Transaction not found');
+
+	const deletedTransactions: Transaction[] = [];
+
+	for (const transaction of transactions) {
+		const deletedTransaction = await deleteTransactionTransaction(
+			transaction as Transaction
+		);
+
+		deletedTransactions.push(deletedTransaction as Transaction);
+	}
+
+	return transactionSchema.parse(deletedTransactions);
 };
 
 export const getTransactionsByFinancialAccountId = async (
@@ -65,4 +114,47 @@ export const getTransactionsByFinancialAccountId = async (
 		include: { category: true }
 	});
 	return transactions;
+};
+
+export const searchTransactions = async (
+	transactionSearchParams: TransactionSearchParams
+) => {
+	let whereFilter = {};
+
+	if (transactionSearchParams.categoryId) {
+		whereFilter = {
+			...whereFilter,
+			categoryId: { equals: transactionSearchParams.categoryId }
+		};
+	}
+
+	if (transactionSearchParams.financialAccountId) {
+		whereFilter = {
+			...whereFilter,
+			financialAccountId: { equals: transactionSearchParams.financialAccountId }
+		};
+	}
+
+	const transactions = await db.transaction.findMany({
+		where: {
+			financialAccount: { userId: { equals: transactionSearchParams.userId } },
+			...whereFilter
+		},
+		include: {
+			category: { select: { name: true } },
+			financialAccount: { select: { name: true } }
+		}
+	});
+
+	return transactionSchema.array().parse(
+		transactions.map(transaction => ({
+			...transaction,
+
+			categoryId:
+				transaction.categoryId === null ? undefined : transaction.categoryId, // Why it throws zodError without this?
+			categoryName: transaction.category?.name,
+			financialAccountName: transaction.financialAccount?.name,
+			dateString: transaction.datetime.toLocaleDateString()
+		}))
+	);
 };
--- src/validators/transaction.ts
+++ src/validators/transaction.ts
@@ -1,14 +1,24 @@
 import z from 'zod';
 
+// TODO: rename datetime to date
 export const transactionCreateSchema = z.object({
 	name: z.string().optional(),
 	description: z.string().optional(),
-	amount: z.number(),
-	datetime: z.date(),
+	amount: z.coerce.number(),
+	datetime: z.coerce.date(),
 	financialAccountId: z.string().min(1, { message: 'Account ID is required' }),
 	categoryId: z.string().optional()
 });
 
 export const transactionSchema = transactionCreateSchema.extend({
-	id: z.string().min(1, { message: 'ID is required' })
+	id: z.string().min(1, { message: 'ID is required' }),
+	categoryName: z.string().optional(),
+	financialAccountName: z.string().optional(),
+	dateString: z.string().optional()
+});
+
+export const transactionSearchSchema = z.object({
+	userId: z.string().optional(),
+	categoryId: z.string().optional(),
+	financialAccountId: z.string().optional()
 });
