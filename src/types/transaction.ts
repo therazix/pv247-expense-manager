1e20f2ca48c8acc1661ce3d75c84081f18cf9752
Jana Stopková <jana.stopkova@intecs.cz>
2023-12-11 18:04:08 +0100

    feat: extend transaction repository

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,12 +1,14 @@
 import { type z } from 'zod';
 
 import {
+	type transactionSearchSchema,
 	type transactionCreateSchema,
 	type transactionSchema
 } from '@/validators/transaction';
 
 export type Transaction = z.infer<typeof transactionSchema>;
 export type NewTransaction = z.infer<typeof transactionCreateSchema>;
+export type TransactionSearchParams = z.infer<typeof transactionSearchSchema>;
 export type TransactionEnhanced = Transaction & {
 	category: {
 		id: string;
