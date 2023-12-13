5adbbffe252366f091d3bf2552f49adf127e92fc
Filip Vágner <therazix@gmail.com>
2023-12-13 12:38:56 +0100

    Add metadata to Transactions page

--- src/app/transactions/layout.tsx
+++ src/app/transactions/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
+
 import TransactionProviders from './transactionProviders';
+
+export const metadata: Metadata = {
+	title: 'Transactions'
+};
 
 const TransactionLayout = ({ children }: { children: React.ReactNode }) => (
 	<TransactionProviders>
